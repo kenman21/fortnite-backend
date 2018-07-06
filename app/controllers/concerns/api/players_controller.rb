104ee1cd93a85db81024ff7e29d52951e98f6e69
Kenny Lehr <kenman9009@gmail.com>
2018-07-06 19:04:14 -0400

    added players/search route

--- app/controllers/concerns/api/players_controller.rb
+++ app/controllers/concerns/api/players_controller.rb
@@ -1,3 +1,7 @@
 class Api::V1::PlayersController < ApplicationController
-  
+
+  def search
+  end 
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :players
+      post 'players/search', to: 'players#search'
+    end
+  end
 end
